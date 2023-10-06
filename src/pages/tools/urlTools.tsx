f98400d02807c7c638ff37df482106048224c6fd
brand.xu <brand.xu@bytesforce.com>
2023-10-06 11:45:27 +0800

    feat: eslint 配置

--- src/pages/tools/urlTools.tsx
+++ src/pages/tools/urlTools.tsx
@@ -1,3 +1,5 @@
+import { get } from "lodash";
+
 class UrlTools {
   static get url() {
     return window.location.href;
@@ -6,6 +8,14 @@ class UrlTools {
   static setUrl(url: any) {
     if (!url) return;
     window.location.href = url;
+  }
+
+  static getUrlParams(location: any = window.location) {
+    const querySearch: string = get(location, "search", "");
+    if (querySearch && querySearch.startsWith("?")) {
+      return querySearch.substr(1, querySearch.length - 1);
+    }
+    return "";
   }
 }
 
