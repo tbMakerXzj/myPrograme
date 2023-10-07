da228089f4e1e2050d73ae8447d1da9749cae939
xzj <1638492106@qq.com>
2023-10-07 21:27:08 +0800

    fix: test

--- src/pages/types/declare.d.ts
+++ src/pages/types/declare.d.ts
@@ -1,1 +1,11 @@
-declare module '*.module.less';
+declare module "*.module.less";
+declare module "*.module.scss";
+declare module "*.module.sass";
+declare module "*.module.css";
+declare module "*.svg";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.bmp";
+declare module "*.tiff";
