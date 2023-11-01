919cef41f4b375cc1e2b9d43b6be9b067baaa114
xzj <1638492106@qq.com>
2023-11-01 23:29:35 +0800

    feat:set集合

--- src/pages/jsFolder/dataSource/set.js
+++ src/pages/jsFolder/dataSource/set.js
@@ -1,4 +1,5 @@
-function set() {
+/* eslint-disable no-console */
+function Set() {
   this.dataStore = [];
   this.add = add;
   this.remove = remove;
@@ -6,6 +7,7 @@ function set() {
   this.show = show;
   this.contain = contain;
   this.union = union;
+  this.intersect = intersect;
 }
 
 function add(data) {
@@ -36,16 +38,61 @@ function show() {
 }
 
 function contain(data) {
-  return this.dataStore.indexOf(data) !== -1;
+  return this.dataStore.indexOf(data) > -1 ? true : false;
 }
 
+// 并集
 function union(set2) {
-  var tempSet = new set();
-  var tempArr = this.dataStore.concat(set2.dataStore);
-  for (var i = 0; i < tempArr.length; i++) {
-    if (tempSet.contain(tempArr[i]) === false) {
-      tempSet.add(tempArr[i]);
+  var tempSet = new Set();
+  // var tempArr = this.dataStore.concat(set2.dataStore);
+  for (var i = 0; i < this.dataStore.length; i++) {
+    tempSet.add(this.dataStore[i]);
+  }
+
+  for (var j = 0; j < set2.dataStore.length; j++) {
+    if (!tempSet.contain(set2.dataStore[j])) {
+      tempSet.dataStore.push(set2.dataStore[j]);
     }
   }
   return tempSet;
 }
+
+// 交集
+function intersect(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.dataStore.length; i++) {
+    if (set.contain(this.dataStore[i])) {
+      tempSet.add(this.dataStore[i]);
+    }
+  }
+  return tempSet;
+}
+
+var names = new Set();
+names.add("John");
+names.add("Bob");
+names.add("Mary");
+names.add("Boasd");
+names.add("Joe");
+names.add("Joe1");
+// if (names.add("Bob")) {
+//   console.log("add success");
+// } else {
+//   console.log("add failed");
+// }
+// console.log(names.show());
+
+names.remove("Boasd");
+console.log("name", names.show());
+
+var names2 = new Set();
+names2.add("Tom");
+names2.add("Cat");
+names2.add("Mary");
+names2.add("Bob");
+names2.add("Joe");
+console.log("name2", names2.show());
+var unionSet = names.union(names2);
+console.log("union", unionSet.show(), unionSet.dataStore.length);
+var intersectSet = names.intersect(names2);
+console.log("intersect", intersectSet.show(), intersectSet.dataStore.length);
