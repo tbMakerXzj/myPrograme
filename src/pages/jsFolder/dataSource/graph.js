0e949f0f8801e8c80e8fb46afd09d22d0577c786
xzj <1638492106@qq.com>
2023-11-08 23:35:55 +0800

    feat: 图

--- src/pages/jsFolder/dataSource/graph.js
+++ src/pages/jsFolder/dataSource/graph.js
@@ -8,6 +8,12 @@ function Graph(v) {
   }
   this.addEdge = addEdge;
   this.showGraph = showGraph;
+  this.dfs = dfs;
+  this.bfs = bfs;
+  this.marked = [];
+  for (var i = 0; i < this.vertices; i++) {
+    this.marked[i] = false;
+  }
 }
 
 function addEdge(v, w) {
@@ -18,10 +24,40 @@ function addEdge(v, w) {
 
 function showGraph() {
   for (var i = 0; i < this.vertices; i++) {
-    +" ";
     console.log(i + " -> ");
     for (var j = 0; j < this.adj[i].length; j++) {
-      console.log(this.adj[i][j] + " ");
+      console.log(this.adj[i][j]);
+    }
+  }
+}
+
+function dfs(v) {
+  this.marked[v] = true;
+  if (this.adj[v] !== undefined) {
+    console.log(v);
+  }
+  for (var w in this.adj[v]) {
+    if (!this.marked[w]) {
+      this.dfs(w);
+    }
+  }
+}
+
+function bfs(s) {
+  var queue = [];
+  this.marked[s] = true;
+  queue.push(s);
+  console.log(1111, queue);
+  while (queue.length) {
+    var v = queue.shift();
+    if (this.adj[v] !== undefined) {
+      console.log(v);
+    }
+    for (var w in this.adj[v]) {
+      if (!this.marked[w]) {
+        this.marked[w] = true;
+        queue.push(w);
+      }
     }
   }
 }
@@ -29,6 +65,8 @@ function showGraph() {
 var graph = new Graph(5);
 graph.addEdge(0, 1);
 graph.addEdge(0, 2);
-graph.addEdge(1, 2);
-graph.addEdge(2, 3);
-graph.showGraph();
+graph.addEdge(1, 3);
+graph.addEdge(2, 4);
+// graph.showGraph();
+// graph.dfs(0);
+graph.bfs(0);
