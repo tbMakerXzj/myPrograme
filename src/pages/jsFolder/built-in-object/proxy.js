9f4f46b7fb39b02694c43e8afc0696c6872a557a
brand.xu <brand.xu@bytesforce.com>
2024-01-05 11:55:00 +0800

    feat: proxy

--- src/pages/jsFolder/built-in-object/proxy.js
+++ src/pages/jsFolder/built-in-object/proxy.js
@@ -4,24 +4,76 @@
  * Proxy(target,handler)
  * handler.get
  * handler.set
+ * handler.has
+ * handler.deleteProperty
+ * handler.ownKeys
+ * handler.getOwnPropertyDescriptor
+ * handler.defineProperty
+ * handler.preventExtensions
+ * handler.getPrototypeOf
+ * handler.isExtensions
+ * handler.setPrototypeOf
  *
  *
  */
-const proxy = new Proxy(
-  {},
-  {
-    get: function (target, property, receiver) {
-      console.log(`getting ${property}`, target, property, receiver);
-      return target[property];
-    },
-    set: function (target, property, value, receiver) {
-      console.log(`setting ${property}`);
-      target[property] = value;
-    },
+function invariant(key, action) {
+  if (key[0] === "_") {
+    throw new Error(`Invalid attempt to ${action} private ${key} property`);
   }
-);
+}
 
-proxy.count = 1;
-proxy.count;
-proxy.name = "张三"; //setting name
-console.log(proxy);
+const target = { _prop: "foo", prop: "foo", baz: "tar" };
+
+const desc = {
+  configurable: true,
+  enumerable: true,
+  value: 10,
+};
+
+const proxy = new Proxy(target, {
+  get: function (target, property, receiver) {
+    return target[property];
+  },
+  set: function (target, property, value, receiver) {
+    target[property] = value;
+  },
+  has: function (target, prop) {
+    if (prop[0] === "_") {
+      return false;
+    }
+    return prop in target;
+  },
+  deleteProperty: function (target, property) {
+    invariant(property, "delete");
+    delete target[property];
+    return true;
+  },
+  getOwnPropertyDescriptor: function (target, key) {
+    if (key[0] === "_") {
+      return;
+    }
+    return Object.getOwnPropertyDescriptor(target, key);
+  },
+  defineProperty: function (target, property, descriptor) {
+    console.log(property);
+    return Reflect.defineProperty(target, property, descriptor);
+  },
+  preventExtensions: function (target) {
+    Object.preventExtensions(target);
+    return true;
+  },
+  getPrototypeOf: function (target) {
+    return Object.getPrototypeOf(target);
+  },
+  isExtensible: function (target) {
+    return Object.isExtensible(target);
+  },
+});
+// delete proxy._prop;
+// console.log("_prop" in proxy);
+// console.log(Object.getOwnPropertyDescriptor(proxy, "baz"));
+// console.log(Object.getOwnPropertyDescriptor(proxy, "_prop"));
+// Object.defineProperty(proxy, "_prop", desc);
+console.log(Object.preventExtensions(proxy));
+// console.log(proxy);
+console.log(Object.isExtensible(proxy));
